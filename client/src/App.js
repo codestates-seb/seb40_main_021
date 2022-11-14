2e86005df7d1de59153b5f1edece5b9720577228
Icyeong <dbcjswjs@naver.com>
2022-11-15 00:37:10 +0900

    ✨ Feat: router설정 완료

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,23 @@
-import OrderAlarms from "./components/AdminComponents/StoreAlarm/OrderAlarms";
+import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Modal } from './components/menu/Modal';
+import { Home } from './pages/menu/Home';
+import { Order } from './pages/menu/Order';
+import { Store } from './pages/menu/Store';
 function App() {
-  return <div className="App"><OrderAlarms></OrderAlarms></div>;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/order/*" element={<Order />} />
+        </Routes>
+      </BrowserRouter>
+      {isModalOpen && <Modal setIsModalOpen={setIsModalOpen} />}
+    </>
+  );
 }
 
 export default App;
