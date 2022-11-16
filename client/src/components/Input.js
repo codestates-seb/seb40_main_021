764dd3b868a0c2466ea0b43a594383155e42d422
Ryu heekyung <beals1129@gmail.com>
2022-11-16 15:53:10 +0900

    💄 Style: Input Compo layout 수정

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -10,10 +10,13 @@ const InputBox = styled.input`
       font-size: 1.4rem;
       font-weight: 400;
    }
-   input {
       font-size: 1.4rem;
-      margin: 8px 0 0;
+      margin: 0 0;
       border-radius: 5px 5px 0 0;
+      padding: 0.6rem 1rem;
+      height: 48px;
+      box-sizing: border-box;
+      
       &::placeholder {
          color: #6d6d6d;
       }
@@ -27,14 +30,14 @@ const InputBox = styled.input`
          border: none;
          border-bottom: 3px solid #666666;
       }
-   }
+
    @media screen and (max-width: 600px) {
-      input[type='text'] {
+      &[type='text'] {
          background: #f4f4f4;
          border: none;
          border-bottom: 0px solid #b6b6b6;
       }
-      input[type='password'] {
+      &[type='password'] {
          background: #f4f4f4;
          border: none;
          border-bottom: 0px solid #666666;
@@ -43,15 +46,15 @@ const InputBox = styled.input`
 `;
 
 const Input = ({ type, value, handleValue, width, placeholders }) => {
-   return (
-      <InputBox
-         type={type}
-         defaultValue={value}
-         onChange={e => handleValue(e.target.value)}
-         width={width}
-         placeholders={placeholders}
-      />
-   );
+  return (
+    <InputBox
+      type={type}
+      //  defaultValue={value}
+      onChange={e => handleValue(e.target.value)}
+      width={width}
+      placeholders={placeholders}
+    />
+  );
 };
 
 export default Input;
