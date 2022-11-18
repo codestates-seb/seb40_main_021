8021e551a3529674896b556ab3e2ff4bcc653c65
Ryu heekyung <beals1129@gmail.com>
2022-11-18 13:58:03 +0900

    💄 Style: Input Style 수정

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -13,18 +13,24 @@ const InputBox = styled.input`
       font-size: 1.4rem;
       margin: 0 0;
       border-radius: 5px 5px 0 0;
-      padding: 0.6rem 1rem;
+      padding: 16px 20px ;
+      font-size: 14px;
       height: 48px;
+      :focus {
+         outline: none !important; border-color: black;
+      }
       box-sizing: border-box;
-      
+      &[type="number"]::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+}
       &::placeholder {
          color: #6d6d6d;
       }
-      &[type='text'] {
          background: #f4f4f4;
          border: none;
          border-bottom: 3px solid #b6b6b6;
-      }
+      
       &[type='password'] {
          background: #f4f4f4;
          border: none;
@@ -32,11 +38,11 @@ const InputBox = styled.input`
       }
 
    @media screen and (max-width: 600px) {
-      &[type='text'] {
+
          background: #f4f4f4;
          border: none;
          border-bottom: 0px solid #b6b6b6;
-      }
+
       &[type='password'] {
          background: #f4f4f4;
          border: none;
@@ -45,16 +51,19 @@ const InputBox = styled.input`
    }
 `;
 
-const Input = ({ type, value, handleValue, width, placeholders }) => {
-  return (
-    <InputBox
-      type={type}
-      //  defaultValue={value}
-      onChange={e => handleValue(e.target.value)}
-      width={width}
-      placeholders={placeholders}
-    />
-  );
+const Input = ({ type, name, handleValue, width, value, placeholder, idx }) => {
+   return (
+      <InputBox
+         type={type}
+         idx={idx}
+         value={value}
+         //  defaultValue={value}
+         onChange={e => handleValue(e)}
+         width={width}
+         placeholder={placeholder}
+         name={name}
+      />
+   );
 };
 
 export default Input;
