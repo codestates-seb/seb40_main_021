8072cf377653810ecbb5284ad7d8a5335a579607
opc13579 <opc13579@gmail.com>
2022-11-20 18:59:27 +0900

    ✨ Feat: 가게 정보 기능 추가

--- client/src/pages/Signup/StoreInfo.js
+++ client/src/pages/Signup/StoreInfo.js
@@ -1,13 +1,63 @@
 import React from 'react';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
 import { Link } from 'react-router-dom';
-import { Active, BtnDefault, BtnDefaultActive, BtnDefaultMobile, CompanyNum, Container, DivideLine, FormControl, ImagePreview, ImgReg, ImgRegBtn, InfoForm, MemberPanel, MemberReg, PageTitle, PanelTitle, RealUpload, Upload, Wrapper } from './StoreInfo.Style';
+import MenuImg from '../../components/Menu/MenuImg';
+import { Active, BtnDefault, BtnDefaultActive, BtnDefaultMobile, CompanyNum, Container, DivideLine, FormControl, ImagePreview, ImgRegBtn, InfoForm, MemberPanel, MemberReg, PageTitle, PanelTitle, RealUpload, Upload, Wrapper } from './StoreInfo.Style';
+
+
+//가게 사진, 가게 설명, 주소, 전화번호, 영업시간
 
 const StoreInfo = () => {
+    
+   const navigate = useNavigate();
+   const [img, setImg] = useState();
+   const [about, setAbout] = useState();
+   const [address, Address] = useState();
+   const [contactNumber, setContactNumber] = useState();
+   const [businessHours, setBusinessHours] = useState();
+ 
+   const postStoreInfo = async () => {
+     try {
+       await axios
+         .post(
+           `${process.env.REACT_APP_API_URL}/StoreInfo`,
+           {
+            img: img,
+            about: about,
+            address: address,
+            contactNumber: contactNumber,
+            businessHours: businessHours,
+           },
+         )
+         .then((res) => navigate(`/StoreInfo/${res.data.id}`));
+     } catch (err) {
+       console.log(err);
+     }
+   };
+
+
+   const [Number, setNumber] = React.useState('');
+   const NumberError = e => {
+      Number(e.target.value);
+
+      const NumberRegex = /^[0-9]+$/;
+
+      if (NumberRegex.test(e.target.value) || e.target.value === '') {
+         NumberRegex(false);
+         NumberError(false);
+      } else {
+         NumberRegex(true);
+         setNumber(true)
+      }
+   };
+
    function getImageFiles(e) {
       const uploadFiles = [];
       const files = e.currentTarget.files;
       const imagePreview = document.querySelector('.image-preview');
-      const docFrag = new DocumentFragment();
+
 
       if ([...files].length >= 2) {
          alert('이미지는 최대 2개 까지 업로드가 가능합니다.');
@@ -50,6 +100,7 @@ const StoreInfo = () => {
    // upload.addEventListener("click", () => realUpload.click());
 
    // realUpload.addEventListener("change", getImageFiles);
+   
    return (
       <Wrapper>
          <Container>
@@ -74,15 +125,23 @@ const StoreInfo = () => {
                   <InfoForm>
                      <label>프로필 사진 등록</label>
                   </InfoForm>
-                  <RealUpload type="file" accept="image/*" required multiple />
-                  <ImgReg>
+                  <RealUpload
+                  type="file"
+                  accept="image/*"
+                  required
+                  multiple
+                  handleValue={setImg} />
+                  <MenuImg>
                      <Upload />
                      <ImagePreview />
-                  </ImgReg>
+                  </MenuImg>
 
                   <InfoForm>
                      <label>가게 설명 등록</label>
-                     <FormControl type="text" placeholder="가게 설명을 입력해주세요" />
+                     <FormControl
+                     type="text"
+                     placeholder="가게 설명을 입력해주세요"
+                     handleValue={setAbout} />
                   </InfoForm>
 
                   <InfoForm>
@@ -94,22 +153,22 @@ const StoreInfo = () => {
                            <img src="images/search.png" />
                         </BtnDefaultMobile>
                      </CompanyNum>
-                     <FormControl type="text" placeholder="상세 주소를 입력해주세요'" />
+                     <FormControl type="text" placeholder="상세 주소를 입력해주세요" />
                   </InfoForm>
 
                   <InfoForm>
                      <label>가게 전화번호 *</label>
-                     <FormControl type="text" placeholder="전화번호를 입력해주세요" />
+                     <FormControl type="text" placeholder="전화번호를 입력해주세요"  handleValue={setContactNumber}/>
                   </InfoForm>
-
+                  {NumberError && <span>숫자만 입력해주세요</span>}
                   <InfoForm>
-                     <label>가게 영업시간 *</label>
-                     <FormControl type="text" placeholder="가게 영업시간을 작성해 주세요" />
+                     <label>가게 영업시간</label>
+                     <FormControl type="text" placeholder="가게 영업시간을 작성해 주세요" handleValue={setBusinessHours} />
                   </InfoForm>
 
                   <ImgRegBtn>
                      <BtnDefaultActive>
-                        <Link to="/complete">완료</Link>
+                        <Link to="/complete" handleSubmit={postStoreInfo}>완료</Link>
                      </BtnDefaultActive>
                   </ImgRegBtn>
                </MemberPanel>
