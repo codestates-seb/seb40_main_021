e857b0a3fe92a47c8c9d8fce67537bf65ca48c73
Icyeong <dbcjswjs@gmail.com>
2022-11-23 15:35:45 +0900

    ♻️ Refactor: 리듀서분리 및 리팩토링

---
+++ client/src/redux/reducers/stateReducer.js
@@ -0,0 +1,29 @@
+import {
+  CATEGORY_SELECTED,
+  MENU_SELECTED,
+  SAVE_MENUID
+} from '../actions/menuAction';
+
+const nonFuncState = {
+  menuId: null,
+  bottomModal: false,
+  category: 1
+};
+
+// 사용자페이지(메뉴판)에서 기타 상태관리를 위한 리듀서
+export const stateReducer = (state = nonFuncState, action) => {
+  switch (action.type) {
+    // 선택한 메뉴 아이디 저장
+    case SAVE_MENUID:
+      return { ...state, menuId: action.payload };
+    // 메뉴 상세 모달 active
+    case MENU_SELECTED:
+      return { ...state, bottomModal: action.payload };
+    //  선택한 카테고리
+    case CATEGORY_SELECTED:
+      return { ...state, category: action.payload };
+
+    default:
+      return state;
+  }
+};
