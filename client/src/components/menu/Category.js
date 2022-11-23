926ca2df4d10db10076ed39a4cbf9ba04a0da4a0
Icyeong <dbcjswjs@gmail.com>
2022-11-23 15:29:40 +0900

    ✨ Feat: 모든 통신테스트 완료

--- client/src/components/menu/Category.js
+++ client/src/components/menu/Category.js
@@ -1,17 +1,35 @@
+import axios from 'axios';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { activateCategory } from '../../redux/actions/menuAction';
+import { activateCategory, setMenu } from '../../redux/actions/menuAction';
 
 export const Category = ({ data }) => {
-  const active = useSelector((store) => store.categoryReducer);
+  const active = useSelector((store) => store.stateReducer.category);
   const dispatch = useDispatch();
+
+  // 클릭한 카테고리 저장
+  const categoryHandler = async () => {
+    dispatch(activateCategory(data.categoryId));
+  };
+
+  // 카테고리별 메뉴목록 불러오기
+  useEffect(() => {
+    axios
+      .get(`/category/read/${active}`)
+      .then((res) => {
+        const menus = res.data.data.menus;
+        dispatch(setMenu(menus));
+      })
+      .catch((err) => console.log(err));
+  }, [active]);
 
   return (
     <li>
       <button
-        onClick={() => dispatch(activateCategory(data.id))}
-        className={active === data.id ? 'active' : null}
+        onClick={categoryHandler}
+        className={active === data.categoryId ? 'active' : null}
       >
-        {data.name}
+        {data.categoryName}
       </button>
     </li>
   );
--- client/src/components/menu/Search.js
+++ client/src/components/menu/Search.js
@@ -1,15 +1,46 @@
-import { useRef } from 'react';
+import axios from 'axios';
+import { useRef, useState } from 'react';
+import { setMenu } from '../../redux/actions/menuAction';
+import { useDispatch } from 'react-redux';
 import IconSearch from './../../assets/image/search.svg';
 
 export const Search = () => {
-  const wrapperRef = useRef();
+  const dispatch = useDispatch();
 
+  const wrapperRef = useRef();
+  const [searchTxt, setSearchTxt] = useState('');
+
+  // 검색하기
   const searchHandeler = () => {
-    wrapperRef.current.classList.toggle('active');
+    if (searchTxt) {
+      axios({
+        method: 'GET',
+        url: `/menu/search/1?keyword=${searchTxt}`
+      })
+        .then((res) => {
+          if (res.data.data.length === 0) {
+            return alert('검색한 메뉴가 없습니다.');
+          }
+          dispatch(setMenu(res.data.data));
+        })
+        .catch((err) => console.log(err));
+
+      setSearchTxt('');
+      wrapperRef.current.classList.remove('active');
+    } else {
+      wrapperRef.current.classList.toggle('active');
+    }
   };
+
   return (
     <div className="search-wrapper" ref={wrapperRef}>
-      <input type="text" name="search" placeholder="메뉴를 입력해주세요." />
+      <input
+        type="text"
+        name="search"
+        placeholder="메뉴를 입력해주세요."
+        value={searchTxt}
+        onChange={(e) => setSearchTxt(e.target.value)}
+      />
       <button className="search-btn" onClick={searchHandeler}>
         <img src={IconSearch} alt="search" />
       </button>
