6157afda82f1d0e1475619ec915c8cb0a0b89317
opc13579 <opc13579@gmail.com>
2022-11-25 00:38:22 +0900

    ✨ Feat: userMemberReducer 추가

---
+++ client/src/redux/reducers/userMemberReducer.js
@@ -0,0 +1,33 @@
+import { USER_MEMBER_ID, USER_MEMBER_PASSWORD, USER_MEMBER_BUSINESSNUMBER } from '../action/action';
+
+const initialState = {
+   id: '',
+   password: '',
+   businessNumber: ''
+};
+
+const userMemberReducer = (state = initialState, action) => {
+   switch (action.type) {
+      case USER_MEMBER_ID:
+         return {
+            ...state,
+            id: action.payload
+         };
+
+      case USER_MEMBER_PASSWORD:
+         return {
+            ...state,
+            password: action.payload
+         };
+
+      case USER_MEMBER_BUSINESSNUMBER:
+         return {
+            ...state,
+            businessNumber: action.payload
+         };
+      default:
+         return state;
+   }
+};
+
+export default userMemberReducer;
