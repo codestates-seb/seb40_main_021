e27bf94959c97f89950a2b60d963626624951621
beomJoon <ehdfkd2000@naver.com>
2022-11-26 14:58:40 +0900

    💄 Style: css 수정

--- client/src/components/AdminComponents/createTable/CreateTable.js
+++ client/src/components/AdminComponents/createTable/CreateTable.js
@@ -38,7 +38,6 @@ const CreateQR = () => {
 const MainContants = styled.div`
    display: flex;
    flex-direction: column;
-   justify-content: center;
    align-items: center;
    height: 90%;
    width: 100%;
