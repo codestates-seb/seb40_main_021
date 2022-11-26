505b76353a179360b943580e81b20996bc76a39b
Icyeong <dbcjswjs@gmail.com>
2022-11-27 01:57:57 +0900

    chore

--- client/src/redux/reducers/adminReducer.js
+++ client/src/redux/reducers/adminReducer.js
@@ -14,7 +14,7 @@ import {
    GET_QR_DATA
 } from '../action/action';
 const adminState = {
-   apiUrl: 'https://3784-118-103-212-116.jp.ngrok.io',
+   apiUrl: 'https://e78d-118-103-212-116.jp.ngrok.io',
    printModal: false,
    qrListAllCheck: false,
    storeInfoUpdateState: false,
