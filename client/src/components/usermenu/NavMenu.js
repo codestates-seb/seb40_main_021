f403a8d5dda4368a34065bd337cf37ed58d02508
Icyeong <dbcjswjs@gmail.com>
2022-11-30 01:43:36 +0900

    🚑 FIX: 통신순서 수정

--- client/src/components/usermenu/NavMenu.js
+++ client/src/components/usermenu/NavMenu.js
@@ -3,25 +3,26 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Category } from './Category';
 import { useEffect } from 'react';
 import { setCategory, setMenu } from '../../redux/actions/menuAction';
-import axios, { all, spread } from 'axios';
+import axios from 'axios';
 import { useParams } from 'react-router-dom';
 
 export const NavMenu = () => {
    const dispatch = useDispatch();
    const category = useSelector(store => store.menuReducer.category);
+   const categoryId = useSelector(store => store.menuReducer.category[0].categoryId);
    const userId = useParams.userId;
 
-   // 카테고리목록 불러오기
-   // 첫번째 카테고리 메뉴목록 불러오기
    useEffect(() => {
-      all([axios.get(`/category/${userId}`), axios.get('/category/read/1')])
-         // all([axios.get('/category/1'), axios.get('/category/read/1')])
+      // 카테고리목록 불러오기
+      axios
+         .get(`/category/${userId}`)
+         .then(res => {
+            dispatch(setCategory(res.data));
+         })
          .then(
-            spread((res1, res2) => {
-               const categoryList = res1.data;
-               const menus = res2.data.data.menus;
-               dispatch(setCategory(categoryList));
-               dispatch(setMenu(menus));
+            // 첫번째 카테고리 메뉴목록 불러오기
+            axios.get(`/category/read/${categoryId}`).then(res => {
+               dispatch(setMenu(res.data.data.menus));
             })
          )
          .catch(err => console.log(err));
