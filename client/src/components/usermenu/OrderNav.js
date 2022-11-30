d4bea26ae693c77fcb884aa973ac2e218bcf5baf
Icyeong <dbcjswjs@gmail.com>
2022-11-30 19:45:12 +0900

    🐛 BUG: params에 따른 path 수정

--- client/src/components/usermenu/OrderNav.js
+++ client/src/components/usermenu/OrderNav.js
@@ -2,19 +2,23 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUtensils, faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export const OrderNav = () => {
+   const params = useSelector(store => store.stateReducer.params);
+
+   console.log(params);
    return (
       <div className="orderNav-wrapper">
          <NavLink
             end
-            to="/usermenu/:userId/:tableNumber/order"
+            to={`/usermenu/${params[0]}/${params[1]}/order`}
             className={({ isActive }) => (isActive ? 'active button' : 'button')}>
             <FontAwesomeIcon icon={faCartShopping} />
             장바구니
          </NavLink>
          <NavLink
-            to="/usermenu/:userId/:tableNumber/order/list"
+            to={`/usermenu/${params[0]}/${params[1]}/order/list`}
             className={({ isActive }) => (isActive ? 'active button' : 'button')}>
             <FontAwesomeIcon icon={faUtensils} />
             주문한 내역
