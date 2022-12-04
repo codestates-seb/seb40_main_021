aaa9006532188f070e26b51bb648aa04987f6934
Ryu heekyung <beals1129@gmail.com>
2022-12-04 14:42:38 +0900

    scroll 막음

--- client/src/components/Preview/PreviewModal.js
+++ client/src/components/Preview/PreviewModal.js
@@ -15,62 +15,62 @@ const PreviewLayout = styled.div`
 `;
 const PreviewModal = ({ now }) => {
    const hi = useRef();
-   // var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+   var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
 
-   // function preventDefault(e) {
-   //    e.preventDefault();
-   // }
+   function preventDefault(e) {
+      e.preventDefault();
+   }
 
-   // function preventDefaultForScrollKeys(e) {
-   //    if (keys[e.keyCode]) {
-   //       preventDefault(e);
-   //       return false;
-   //    }
-   // }
+   function preventDefaultForScrollKeys(e) {
+      if (keys[e.keyCode]) {
+         preventDefault(e);
+         return false;
+      }
+   }
 
-   // // modern Chrome requires { passive: false } when adding event
-   // var supportsPassive = false;
-   // try {
-   //    window.addEventListener(
-   //       'test',
-   //       null,
-   //       Object.defineProperty({}, 'passive', {
-   //          // eslint-disable-next-line getter-return
-   //          get: function () {
-   //             supportsPassive = true;
-   //          }
-   //       })
-   //    );
-   // } catch (e) {
-   //    e;
-   // }
+   // modern Chrome requires { passive: false } when adding event
+   var supportsPassive = false;
+   try {
+      window.addEventListener(
+         'test',
+         null,
+         Object.defineProperty({}, 'passive', {
+            // eslint-disable-next-line getter-return
+            get: function () {
+               supportsPassive = true;
+            }
+         })
+      );
+   } catch (e) {
+      e;
+   }
 
-   // var wheelOpt = supportsPassive ? { passive: false } : false;
-   // var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+   var wheelOpt = supportsPassive ? { passive: false } : false;
+   var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
 
-   // // call this to Disable
-   // function disableScroll() {
-   //    window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
-   //    window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
-   //    window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
-   //    window.addEventListener('keydown', preventDefaultForScrollKeys, false);
-   // }
+   // call this to Disable
+   function disableScroll() {
+      window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+      window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+      window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+      window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+   }
 
-   // // call this to Enable
-   // function enableScroll() {
-   //    window.removeEventListener('DOMMouseScroll', preventDefault, false);
-   //    window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
-   //    window.removeEventListener('touchmove', preventDefault, wheelOpt);
-   //    window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
-   // }
+   // call this to Enable
+   function enableScroll() {
+      window.removeEventListener('DOMMouseScroll', preventDefault, false);
+      window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+      window.removeEventListener('touchmove', preventDefault, wheelOpt);
+      window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+   }
    useEffect(() => {
-      //scroll top
+      // scroll top
       window.scrollTo(0, 0);
       // modal이 떠 있을 땐 스크롤 막음
-      // disableScroll();
+      disableScroll();
 
       // modal 닫히면 다시 스크롤 가능하도록 함
-      // return () => enableScroll();
+      return () => enableScroll();
    }, []);
 
    return (
